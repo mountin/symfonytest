fdf25c1974c854bdf7cb1d02b4f85df64e1c7beb
julienjovy <julien.jovy@gmail.com>
2024-11-27 14:08:37 +0100

    I missed some useless code which was blocking

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -24,8 +24,8 @@ class Author
     /**
      * @var Collection<int, Book>
      */
-//    #[ORM\OneToMany(targetEntity: Book::class, mappedBy: 'author')]
-//    private Collection $books;
+    //    #[ORM\OneToMany(targetEntity: Book::class, mappedBy: 'author')]
+    //    private Collection $books;
 
     /**
      * @var Collection<int, Book>
@@ -72,35 +72,5 @@ class Author
     public function __toString()
     {
         return $this->name;
-    }
-
-    /**
-     * @return Collection<int, Book>
-     */
-    public function getBooks2(): Collection
-    {
-        return $this->books2;
-    }
-
-    public function addBooks2(Book $books2): static
-    {
-        if (!$this->books2->contains($books2)) {
-            $this->books2->add($books2);
-            $books2->setAuthorId($this);
-        }
-
-        return $this;
-    }
-
-    public function removeBooks2(Book $books2): static
-    {
-        if ($this->books2->removeElement($books2)) {
-            // set the owning side to null (unless already changed)
-            if ($books2->getAuthorId() === $this) {
-                $books2->setAuthorId(null);
-            }
-        }
-
-        return $this;
     }
 }
